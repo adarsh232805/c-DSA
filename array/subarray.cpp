97acde6d0bc8e99a868c5ba43ce50509de81f21e
Adarsh Singh <adarshsingh097singh@gmail.com>
2025-09-21 14:27:44 +0530

    subarray

--- array/subarray.cpp
+++ array/subarray.cpp
@@ -3,14 +3,16 @@ using namespace std;
 int main(){
     int n=5;
     int arr[n]={1,2,3,4,5};
-    for(int st=0;st<n;st++){
-        for(int end=st;end<n;end++){
-            for(int i=st;i<=end;i++){
-                cout<<arr[i]<<" ";
-            }
-            cout<<" ";
+   for(int start=0;start<n;start++){
+    for(int end=start;end<n;end++){
+        for(int i=start;i<=end;i++){
+            cout<<arr[i];
         }
-       // cout<<endl;
-    }
+        cout<<" ";
+    
+    
+}
+    cout<<endl;
+   }
 
 }
